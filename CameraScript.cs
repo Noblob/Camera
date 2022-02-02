a7e4075a3709e99cc0a0ed9af77cb35c300416ec
Noblob <96800201+Noblob@users.noreply.github.com>
2022-02-02 15:57:33 -0500

    Create CameraScript.cs

---
+++ CameraScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public GameObject Camera1;
+    public GameObject Camera2;
+    public GameObject Camera3;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            CameraOne ();
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            CameraTwo ();
+        }
+
+        if (Input.GetKeyDown("3"))
+        {
+            CameraThree ();
+        }
+    }
+
+    void CameraOne()
+    {
+        Camera1.SetActive(true);
+        Camera2.SetActive(false);
+        Camera3.SetActive(false);
+    }
+
+    void CameraTwo()
+    {
+        Camera3.SetActive(false);
+        Camera2.SetActive(true);
+        Camera1.SetActive(false);  
+    }
+
+    void CameraThree()
+    {
+        Camera3.SetActive(true);
+        Camera2.SetActive(false);  
+        Camera1.SetActive(false);
+    }
+}
